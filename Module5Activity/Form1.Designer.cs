77387f37404ebcace5844aa270e09e01766f7725
Richard Chapman <chapmro@auburn.edu>
2019-02-11 14:59:31 -0600

    file i/o only

--- Module5Activity/Form1.Designer.cs
+++ Module5Activity/Form1.Designer.cs
@@ -28,13 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(509, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "text to search";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 52);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(37, 154);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(641, 204);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Module5Activity/Form1.cs
+++ Module5Activity/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,45 @@ namespace Module5Activity
         public Form1()
         {
             InitializeComponent();
+        }
+
+        StreamReader inputfile;
+        String fileContents = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                inputfile = File.OpenText(openFileDialog1.FileName);
+                while (!inputfile.EndOfStream)
+                {
+                    fileContents = fileContents + (inputfile.ReadLine());
+                }
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                
+            }
+            else
+            { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool done = false;
+            string currLine; 
+            if (fileContents == "")
+            {
+                return;
+            } else
+            {
+                while (!done)
+                {
+                    currLine = 
+                }
+            }
         }
     }
 }
